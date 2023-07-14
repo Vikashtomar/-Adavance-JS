c579e17b9d8eec79a3869333b9187bf4e917a3a7
vikash <vikash.tomar.india@gmail.com>
2023-07-14 18:23:11 +0530

    first commit

---
+++ objectLitrals.js
@@ -0,0 +1,57 @@
+
+
+// let user = "ram";
+// let city = "Agra";
+// let password ="123344";
+
+
+// var emp = {
+
+//     uname: "vikas",
+//     user,
+//     city,
+
+// }
+
+// console.log(emp)
+
+
+
+
+
+// let c = "city";
+
+// let s = "state";
+
+// let data ={
+//     fname: "Vikash Tomar",
+//     [c]: "Mathura",
+//     age : 25,
+//     [s]:"state"
+// }
+// console.log(data)
+
+
+
+// let obj = {
+//     fname: "vikas",
+//     lname: 'tomar',
+//     age:   19,
+//     hello(a,b,c){
+//         // return `hello world`;
+        
+//         return (a+b+c)
+        
+//     }
+    
+// }
+// console.log(obj.hello(10,20,30))
+
+
+let uname="vikas";
+let age = 21;
+function data(username,userage){
+    return {username,userage};
+}
+
+console.log(data(uname,age))
---
+++ strictmode.js
@@ -0,0 +1,9 @@
+// "use strict"
+
+// a = 20;
+// console.log(a)
+
+function sum(a,b,b){
+    console.log(a+b+b);
+}
+sum(10,50,100)
